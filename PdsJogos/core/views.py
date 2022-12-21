f8839fa430681f1f2262335c9564a2a83a6beb59
vitorviana <jvitorviana14@gmail.com>
2022-12-20 23:01:31 -0300

    implementando registro de usuario

--- PdsJogos/core/views.py
+++ PdsJogos/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import UsuarioCreationForm
 
-# Create your views here.
+def registro(request):
+    form = UsuarioCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    contexto = {
+        'form': form
+    }
+    return render(request, 'registro.html', contexto)
