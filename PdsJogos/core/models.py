4196ef575d240c8ac5d0bda7b974a9759455e4d7
Lenivaldo Lucas Ribeiro dos Santos <20191194010015@ifrn.local>
2022-12-22 11:10:52 -0300

    estendendo base para index

--- PdsJogos/core/models.py
+++ PdsJogos/core/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class: Usuario(AbstractUser):
+class Usuario(AbstractUser):
     nome = models.CharField('Nome', max_length=100)
     idade = models.ImageField('Idade')
     email = models.EmailField('Email')
-    matricula = models.CharField('Matricula', max_length=14)
+    matricula = models.CharField('Matricula', max_length=14, unique=True)
     telofone = models.CharField('Telefone', max_length=12)
 
-USERNAME_FILED = 'Nome' 
+    USERNAME_FILED = 'matricula' 
 
 # Create your models here.
