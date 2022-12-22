e24d9bad5eef4c7401805ac51d7e35709feeb0bb
Lenivaldo Lucas Ribeiro dos Santos <20191194010015@ifrn.local>
2022-12-22 10:47:59 -0300

    models

--- PdsJogos/core/models.py
+++ PdsJogos/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class: Usuario(AbstractUser):
+    nome = models.CharField('Nome', max_length=100)
+    idade = models.ImageField('Idade')
+    email = models.EmailField('Email')
+    matricula = models.CharField('Matricula', max_length=14)
+    telofone = models.CharField('Telefone', max_length=12)
+
+USERNAME_FILED = 'Nome' 
 
 # Create your models here.
